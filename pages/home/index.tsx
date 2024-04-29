a0d8832eb8430baa5f55f4665c5f8f89123ce824
Mursal <mursal.haxverdiyev@gmail.com>
2024-04-29 04:08:43 +0400

    bug fixed

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -85,6 +85,8 @@ const Home = () => {
       setMessages(data.messages);
       data.user && setUser(data.user);
       data.otherUsers && setOtherUsers(data.otherUsers);
+      console.log(messages);
+      
     }
   }, [lastMessage]);
 
